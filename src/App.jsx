d5dd84174f5173bc724b0d0ee18e08e8c9011973
jfNatanahel <natanahel.jf@gmail.com>
2025-09-08 11:39:26 -0300

    Proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ export default function Profile(){
     <> 
     <h1>TP1</h1>
     <h2>Jose Natanahel Fernandez</h2>
+    <p>Crear una página HTML que contenga una estructura básica y un botón de
+      tipo File para subir una imagen. La misma tendrá que ser validada que se
+      trata de una imagen y mostrar dentro del documento html el contenido
+      de la misma. Para esta tarea deberá utilizar la API File</p>
       <input 
       type="file" 
       accept='image/*' 
