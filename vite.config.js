f300be34a28550283b5af98b166e99ea6b0d7fca
jfNatanahel <natanahel.jf@gmail.com>
2025-09-08 11:44:58 -0300

    Agregando la propiedad base para lograr tomarlo en deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+/*
+Cuando deployás en GitHub Pages, tu proyecto no se carga desde la raíz (/), sino desde una subcarpeta con el nombre de tu repo.
+Por eso hay que agregar la propiedad base.
+*/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-mi-primer-proyecto/',
 })
